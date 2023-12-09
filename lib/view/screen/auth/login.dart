07c5d219af591e43c40252e505d4a556e82982c9
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-12-09 10:24:21 +0700

    feat: create auth textfield

--- lib/view/screen/auth/login.dart
+++ lib/view/screen/auth/login.dart
@@ -1,5 +1,6 @@
 import 'package:employees_organizer/constants/font_family.dart';
 import 'package:employees_organizer/view/widget/auth/button.dart';
+import 'package:employees_organizer/view/widget/auth/textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,28 +9,35 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            const Text('Sign In',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: poppins)),
-            ButtonAuthWidget(title: 'Sign In', onPressed: () {}),
-            const SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              height: 300,
-              color: Colors.purple,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const Text('Sign In',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: poppins)),
+                TextFieldAuthWidget(
+                    textEditingController: TextEditingController(),
+                    title: 'Email',
+                    placeholderText: 'Type your email',
+                    leadingIcon: Icons.email_outlined),
+                const SizedBox(height: 10),
+                ButtonAuthWidget(title: 'Sign In', onPressed: () {}),
+                const SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  height: 300,
+                  color: Colors.purple,
+                ),
+                const Text('Login'),
+              ],
             ),
-            const Text('Login'),
-          ],
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
--- lib/view/widget/auth/textfield.dart
+++ lib/view/widget/auth/textfield.dart
@@ -1,3 +1,4 @@
+import 'package:employees_organizer/constants/font_family.dart';
 import 'package:flutter/material.dart';
 
 class TextFieldAuthWidget extends StatelessWidget {
@@ -5,16 +6,45 @@ class TextFieldAuthWidget extends StatelessWidget {
   final String title;
   final String placeholderText;
   final IconData leadingIcon;
+  final IconButton? trailingIcon;
 
   const TextFieldAuthWidget(
       {super.key,
       required this.textEditingController,
       required this.title,
       required this.placeholderText,
-      required this.leadingIcon});
+      required this.leadingIcon,
+      this.trailingIcon});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12.0),
+          child: Text(title,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: poppins,
+                  fontSize: 19)),
+        ),
+        const SizedBox(height: 10),
+        TextFormField(
+          onTapOutside: (event) {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          controller: textEditingController,
+          decoration: InputDecoration(
+              contentPadding: const EdgeInsets.symmetric(vertical: 15),
+              hintText: placeholderText,
+              prefixIcon: Icon(leadingIcon),
+              suffixIcon: trailingIcon,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(color: Colors.grey))),
+        ),
+      ],
+    );
   }
 }
