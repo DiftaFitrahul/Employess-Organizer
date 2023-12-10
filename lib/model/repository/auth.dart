e05602a83ba705d61106dfb7bb88adc5a110996e
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-12-10 09:05:20 +0700

    feat: create controller for home screen

--- lib/model/repository/auth.dart
+++ lib/model/repository/auth.dart
@@ -1,14 +1,18 @@
+import 'dart:convert';
+
 import 'package:employees_organizer/model/classModel/user_login.dart';
 import 'package:employees_organizer/model/classModel/user_register.dart';
 import 'package:get/get.dart';
 
 class AuthService extends GetConnect {
   Future<Response> login({required UserLogin model}) => post(
-        'https://reqres.in/api/login',
+      'https://reqres.in/api/login',
+      jsonEncode(
         model.toJson(),
-      );
+      ));
   Future<Response> register({required UserRegister model}) => post(
-        'https://reqres.in/api/register',
+      'https://reqres.in/api/register',
+      jsonEncode(
         model.toJson(),
-      );
+      ));
 }
--- lib/viewModel/home.dart
+++ lib/viewModel/home.dart
@@ -1,3 +1,29 @@
+import 'dart:convert';
+
+import 'package:employees_organizer/model/classModel/get_employee.dart';
+import 'package:employees_organizer/model/repository/employee.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController with StateMixin<List<GetEmployee>> {
+  @override
+  void onInit() {
+    getEmployee();
+    super.onInit();
+  }
+
+  Future<void> getEmployee() async {
+    try {
+      change([], status: RxStatus.loading());
+      final result = await EmployeeService().getEmployee();
+      if (result.status.hasError) {
+        change([], status: RxStatus.error());
+        return;
+      }
+      final data = (jsonDecode(result.body)['data']) as List;
+      change(data.map((e) => GetEmployee.fromJson(e)).toList(),
+          status: RxStatus.success());
+    } catch (e) {
+      change([], status: RxStatus.error());
+    }
+  }
+}
