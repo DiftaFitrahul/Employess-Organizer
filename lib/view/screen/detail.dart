e350da9b203cbc2fc34e8c2e788ead2af8abc37e
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-12-10 08:09:07 +0700

    feat: separate stack in detail screen

--- lib/view/screen/detail.dart
+++ lib/view/screen/detail.dart
@@ -2,6 +2,7 @@ import 'package:employees_organizer/constants/color.dart';
 import 'package:employees_organizer/constants/font_family.dart';
 import 'package:employees_organizer/constants/font_weight.dart';
 import 'package:employees_organizer/view/widget/detail/content.dart';
+import 'package:employees_organizer/view/widget/detail/modal.dart';
 import 'package:employees_organizer/view/widget/detail/user_image.dart';
 import 'package:flutter/material.dart';
 
@@ -10,76 +11,19 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Stack(children: [
-        const Column(
+        Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             UserImageDetailScreen(),
             SizedBox(height: 80),
-            ContentDetailScreen()
+            ContentDetailScreen(),
           ],
         ),
         Align(
-          alignment: AlignmentDirectional.center,
-          child: Container(
-            width: double.infinity,
-            height: 150,
-            margin: const EdgeInsets.symmetric(horizontal: 48),
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0, 2),
-                  blurRadius: 6,
-                )
-              ],
-            ),
-            child: Column(
-              children: [
-                const Text(
-                  'Difta Fitrahul',
-                  style: TextStyle(
-                      fontFamily: poppins, fontSize: 24, fontWeight: bold),
-                ),
-                const Text(
-                  'difta.sleman@mail.sleman.id',
-                  style: TextStyle(
-                      fontFamily: poppins, fontSize: 13, fontWeight: normal),
-                ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: primatyColor,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)),
-                        padding:
-                            EdgeInsets.symmetric(vertical: 12, horizontal: 16)),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          Icons.edit_outlined,
-                          color: Colors.white,
-                        ),
-                        const SizedBox(width: 8),
-                        const Text(
-                          'Edit Contact',
-                          style: TextStyle(
-                              fontFamily: poppins,
-                              fontSize: 16,
-                              color: Colors.white,
-                              fontWeight: semiBold),
-                        )
-                      ],
-                    ))
-              ],
-            ),
-          ),
+          alignment: Alignment.center,
+          child: ModalDetailScreen(),
         )
       ]),
     );
